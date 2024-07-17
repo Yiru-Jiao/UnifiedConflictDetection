036a640b1171efea0fe46cf3ff647943bdc9b592
Yiru Jiao <y.jiao-1@tudelft.nl>
2024-07-17 09:26:45 +0200

    integrate phi computation into warning

--- Demonstration/ConflictProbability/collision_warning.py
+++ Demonstration/ConflictProbability/collision_warning.py
@@ -22,7 +22,7 @@ path_output = './Data/OutputData/'
 # Load data
 events = pd.read_hdf(path_input+'100Car/HundredCar_NearCrashes.h5', key='data')
 meta = pd.read_csv(path_input+'100Car/HundredCar_metadata_NearCrashes.csv')
-proximity_phi = pd.read_csv(path_output+'conflict_probability/proximity_phi.csv')
+
 
 # PSD
 demonstration = []
@@ -34,8 +34,9 @@ for psd_threshold in tqdm(np.round(np.arange(0.04,4.,0.04),2), desc='PSD'):
     meta['threshold'] = psd_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
+print(f'PSD: {np.mean(used_time):.4f} seconds per threshold') # 0.7591s
 demonstration.to_csv(path_output+'conflict_probability/PSD_warning.csv', index=False)
-print(f'PSD: {np.mean(used_time):.4f} seconds per threshold') # 0.6942s
+
 
 # DRAC
 demonstration = []
@@ -47,8 +48,8 @@ for drac_threshold in tqdm(np.round(np.arange(0.05,5.,0.05),2), desc='DRAC'):
     meta['threshold'] = drac_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
+print(f'DRAC: {np.mean(used_time):.4f} seconds per threshold') # 1.0220s
 demonstration.to_csv(path_output+'conflict_probability/DRAC_warning.csv', index=False)
-print(f'DRAC: {np.mean(used_time):.4f} seconds per threshold') # 0.9196s
 
 # TTC
 demonstration = []
@@ -60,11 +61,15 @@ for ttc_threshold in tqdm(np.round(np.arange(0.2,20.,0.2),1), desc='TTC'):
     meta['threshold'] = ttc_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
+print(f'TTC: {np.mean(used_time):.4f} seconds per threshold') # 1.3091s
 demonstration.to_csv(path_output+'conflict_probability/TTC_warning.csv', index=False)
-print(f'TTC: {np.mean(used_time):.4f} seconds per threshold') # 0.9706s
+
 
 # Unified
 demonstration = []
+current_time = systime.time()
+proximity_phi = compute_phi(events, path_output)
+pre_computation_time = systime.time()-current_time
 used_time = []
 n_list = np.arange(2,101)
 for n in tqdm(n_list, desc='Unified'):
@@ -74,5 +79,6 @@ for n in tqdm(n_list, desc='Unified'):
     meta['threshold'] = n
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
+print(f'Unified: {(pre_computation_time+np.mean(used_time)):.4f} seconds per threshold') # 4.4051s)
 demonstration.to_csv(path_output+'conflict_probability/Unified_warning.csv', index=False)
-print(f'Unified: {np.mean(used_time):.4f} seconds per threshold') # 0.5434s
+proximity_phi.to_csv(path_output+'conflict_probability/proximity_phi.csv', index=False)

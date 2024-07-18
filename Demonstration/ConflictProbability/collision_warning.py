0c57ed62bc9ea28276b94babd13938a736bc5f1a
Yiru Jiao <y.jiao-1@tudelft.nl>
2024-07-18 14:56:20 +0200

    update computation time test

--- Demonstration/ConflictProbability/collision_warning.py
+++ Demonstration/ConflictProbability/collision_warning.py
@@ -34,7 +34,7 @@ for psd_threshold in tqdm(np.round(np.arange(0.04,4.,0.04),2), desc='PSD'):
     meta['threshold'] = psd_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
-print(f'PSD: {np.mean(used_time):.4f} seconds per threshold') # 0.7591s
+print(f'PSD: {np.mean(used_time):.4f} seconds per threshold') # 0.9205s
 demonstration.to_csv(path_output+'conflict_probability/PSD_warning.csv', index=False)
 
 
@@ -48,7 +48,7 @@ for drac_threshold in tqdm(np.round(np.arange(0.05,5.,0.05),2), desc='DRAC'):
     meta['threshold'] = drac_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
-print(f'DRAC: {np.mean(used_time):.4f} seconds per threshold') # 1.0220s
+print(f'DRAC: {np.mean(used_time):.4f} seconds per threshold') # 0.9852s
 demonstration.to_csv(path_output+'conflict_probability/DRAC_warning.csv', index=False)
 
 # TTC
@@ -61,7 +61,7 @@ for ttc_threshold in tqdm(np.round(np.arange(0.2,20.,0.2),1), desc='TTC'):
     meta['threshold'] = ttc_threshold
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
-print(f'TTC: {np.mean(used_time):.4f} seconds per threshold') # 1.3091s
+print(f'TTC: {np.mean(used_time):.4f} seconds per threshold') # 1.3176s
 demonstration.to_csv(path_output+'conflict_probability/TTC_warning.csv', index=False)
 
 
@@ -79,6 +79,6 @@ for n in tqdm(n_list, desc='Unified'):
     meta['threshold'] = n
     demonstration.append(meta)
 demonstration = pd.concat(demonstration).reset_index(drop=True)
-print(f'Unified: {(pre_computation_time+np.mean(used_time)):.4f} seconds per threshold') # 4.4051s)
+print(f'Unified: {(pre_computation_time+np.mean(used_time)):.4f} seconds per threshold') # 4.6558s)
 demonstration.to_csv(path_output+'conflict_probability/Unified_warning.csv', index=False)
 proximity_phi.to_csv(path_output+'conflict_probability/proximity_phi.csv', index=False)
